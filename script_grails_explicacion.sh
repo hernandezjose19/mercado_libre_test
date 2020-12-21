243afb5dd7ceda8df815e1bde853975da4a8ada2
Jose Hernandez <68482155+hernandezjose19@users.noreply.github.com>
2020-12-21 16:37:51 -0300

    Update script_grails_explicacion.sh

--- script_grails_explicacion.sh
+++ script_grails_explicacion.sh
@@ -1,3 +1,15 @@
-Este trae com output:
+import me.*;
+def upsPullTrkService = ctx.getBean('upsPullTrkService')
+def s = Shipment.get(27528954729)
+def tn = s.trackingNumber
+def trackingData = upsPullTrkService.getTrkEvents([tn])
+trackingData.each { td ->
+println "------------------------------------------------"
+println "${td.sucursal} - ${td.eventDate} - ${td.description}"
+}
+"Done"
 
-                 sucursal, fecha del evento y descripcion del evento.
+#Este trae com output:
+
+
+# -------- sucursal, fecha del evento y descripcion del evento.
